600ef9f8db9e580f2b984c48e3f8bf701c9adec2
f4rh4nnurA <farhannuralifian1@gmail.com>
2023-11-18 07:42:32 +0700

    jobsheet11

---
+++ Minggu11/Tugas/Nomor2.java
@@ -0,0 +1,18 @@
+package Minggu11.Tugas;
+
+import java.util.Scanner;
+
+public class Nomor2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+        int N ;
+        System.out.print("Masukkan nilai N: ");
+        N = input.nextInt();
+        for(int i = N; i >= 1; i--){
+            for(int j = 1; j <= i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
